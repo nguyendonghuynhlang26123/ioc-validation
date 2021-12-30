3fe93a398a45392ce9d305c8ca1006663b6c44f4
Nguyen Dang Huynh Long <ldh@elcaNet.local>
2021-12-30 21:56:11 +0700

    Draft Collection validator chain2

--- src/main/java/validator/impl/CollectionValidatorChain.java
+++ src/main/java/validator/impl/CollectionValidatorChain.java
@@ -8,15 +8,13 @@ public class CollectionValidatorChain<T> extends CompositeValidatorChain<T>{
     @Override
     public void processValidation(ValidateObject<T> value, ViolationContext context) {
         if (Collection.class.isAssignableFrom(value.getType())){
-            Collection collection = (Collection) value.getValue();
+            Collection<T> collection = (Collection<T>) value.getValue();
             collection.forEach(
                     element -> {
-                        ValidateObject validateObject = new ValidateObject(element.getClass(), element);
-                        chains.forEach((k, chainList) -> {
-                            chainList.forEach(chain -> chain.processValidation(validateObject, context));
-                        });
+                        super.processValidation(new ValidateObject<T>((Class<T>) element.getClass(), element), context);
                     }
             );
         }
     }
+
 }
